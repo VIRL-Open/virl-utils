77bb3a4c2c11b42d51558a1de021d152087b5186
Ralph Schmieder <rschmied@cisco.com>
2015-01-29 13:32:47 +0100

    launch file and curl examples update

--- API/launch-topo.py
+++ API/launch-topo.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
-import requests
+import os, requests, sys
 
-virl_host = "172.16.1.1"
-username = password = "guest"
-url = "http://%s:19399/simengine/rest/launch" % virl_host
-topo = open('example.virl', 'r')
-headers = {'content-type': 'text/xml'}
+def launch_topo(topofilename):
+	virl_host = "172.16.1.1"
+	username  = password = "guest"
+	url       = "http://%s:19399/simengine/rest/launch" % virl_host
+	topo      = open(topofilename, 'r')
+	headers   = {'content-type': 'text/xml'}
+	payload   = {'file': 'manual@launch_topo'}
 
-result = requests.post(url, auth=(username, password), data=topo, headers=headers)
-print result.text
+	result = requests.post(url, auth=(username, password), params=payload, data=topo, headers=headers)
+	print result.text
+
+
+def main():
+        if len(sys.argv) != 2:
+                sys.stdout.write(str(sys.argv[0]))
+                print ": topology filename needed as argument! bailing out"
+                return 1                
+        else:
+                launch_topo(str(sys.argv[1]).strip())
+                return 0
+
+
+if __name__ == '__main__':
+        sys.exit(main())
+
+
+
