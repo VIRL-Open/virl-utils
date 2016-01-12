727abb5444e9cbdbdd1e5eb92ef247fc76d00726
Ralph Schmieder <rschmied@cisco.com>
2016-01-12 08:36:10 +0100

    a bit of overhaul here and there
    - changed regex to work with webeditor topologies (w/o 6-digit random id)
    - added IP column for non-infrastructure ports
    - changed 'user' to 'project' because it is
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- list.py
+++ list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 #
 # Lists all interfaces of all VMs run by a particular
-# user. The username has to provided as a paramater.
+# project. The project name can be provided as a paramater.
+# If none is provided, all projects are listed.
+#
 # The tap interfaces can then be used to do a traffic
 # capture locally using tcpdump
 #
+# v0.3 02-Dec-2015 
+# - changed regex to work with webeditor topologies (w/o 6-digit random id)
+# - added IP column for non-infrastructure ports
+# - changed 'user' to 'project' because it is
 # v0.2 02-Dec-2015 
 # adapted to Kilo release
 # v0.1 24-Nov-2014
@@ -17,57 +23,63 @@ import sys, re, os, prettytable
 from neutronclient.v2_0 import client
 from operator import itemgetter
 
+# </guest/endpoint>-<triangle-ghU06c>-<iosv-2>-<guest> 
+# </guest/endpoint>-<trunk>-<lxc-2>-<iosvl2-1-to-lxc-2>
+# </guest/endpoint>-<triangle-ghU06c>-<~mgmt-lxc>-<~lxc-flat>
+# </guest/endpoint>-<jjj>-<~mgmt-lxc>-<~lxc-flat>
+# </guest/endpoint>-<testAA-u6dRq3>-<lxc-1>-<guest>
 
-##	 </guest/endpoint>-<testAA-u6dRq3>-<lxc-1>-<guest>
-
-def list_taps(user):
+def list_taps(project):
 	out = {}
 
 	nc = client.Client(username=os.environ['OS_USERNAME'], password=os.environ['OS_PASSWORD'],
 		tenant_name=os.environ['OS_TENANT_NAME'], auth_url=os.environ['OS_SERVICE_ENDPOINT'])
 
-	prog = re.compile(r'</('+user+')/endpoint>-<(.*)-[0-9a-zA-Z_]{6}>-<(.*)>-<(.*)>')
+	prog = re.compile(r'</('+project+')/endpoint>-<(.*)(?:-\w{6})?>-<(.*)>-<(.*)>')
 	ports = nc.list_ports()
 	for thisport in ports['ports']:
 		m = prog.match(thisport['name'])
-
 		if m:
-			(user, topo, node, connection) = (m.group(1), m.group(2), m.group(3), m.group(4))
-
-			if not user in out:
-				out[user] = {}
-			if not topo in out[user]:
-				out[user][topo] = []
-			if connection == user:
+			project, topo, node, connection = m.group(1), m.group(2), m.group(3), m.group(4)
+			if not project in out:
+				out[project] = {}
+			if not topo in out[project]:
+				out[project][topo] = []
+			if connection == project:
 				tmp = 'Management Network'
 			else:
 				tmp = connection
-			out[user][topo].append({'id': thisport['id'], 
-				'from': node, 'to': tmp, 'mac': thisport['mac_address']})
+			ip = thisport['fixed_ips'][0]['ip_address']
+			if ip in ['10.255.255.1', '10.255.255.2']:
+				ip = ''
+			out[project][topo].append({'id': thisport['id'], 
+				'from': node, 'to': tmp, 'mac': thisport['mac_address'], 'ip': ip})
 
-	pt = prettytable.PrettyTable(["Project", "Topology", "Node", "Link", "Interface", "MAC Address"])
+	pt = prettytable.PrettyTable(["Project", "Topology", "Node", "Link", "Interface", "MAC Address", "IP Address"])
 	pt.align = "l"
 	
-	for proj in out:
-		projectname = proj.title()
-		for topo in out[proj]:
-			toponame = topo.title()
-			out[proj][topo].sort(key=itemgetter('from'))
-			for port in out[proj][topo]:
+	for project in out:
+		projectname = project
+		for topo in out[project]:
+			toponame = topo
+			out[project][topo].sort(key=itemgetter('from'))
+			for port in out[project][topo]:
 				pt.add_row([projectname, toponame, port['from'], 
 					port['to'], "tap" + port['id'][0:11], port['mac'], port['ip']])
-				projectname = ""
-				toponame = ""
-	print pt.get_string()
+				if len(toponame) > 0:
+					toponame = ""
+					projectname = ""
+	print pt
 	return 0
 
 
 def main():
-	user='.*'
+	project='.*'
 	if len(sys.argv) == 2:
-		user=str(sys.argv[1]).strip()
-	return list_taps(user)
+		project=str(sys.argv[1]).strip()
+	return list_taps(project)
 
 
 if __name__ == '__main__':
 	sys.exit(main())
+
