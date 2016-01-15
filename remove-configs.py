49166679f75301168fb3c00092c206456803e49c
Ralph Schmieder <rschmied@cisco.com>
2016-01-15 22:38:37 +0100

    rewrite
    added ability to remove all the node interface IPs and masks to give ANK a chance to re-configure from scratch
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- remove-configs.py
+++ remove-configs.py
@@ -1,29 +1,58 @@
 #!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=0 noexpandtab
 
-from __future__ import print_function
-from lxml import etree as ElementTree
-import sys
+"""
+Copyright (c) 2014, Cisco
 
-def walk(root, depth):
-	# print('\t'*depth, "tag=", root.tag, "keys=", root.keys(), sep='')
-	for child in root:
-		if len(child) > 0:
-			walk(child, depth+1)
-		else:
-			# print('\t'*(depth+1), end='')
-			# print(child.tag, child.keys())
-			if child.tag.lower().endswith('entry') and \
-			   child.getparent().tag.lower().endswith('extensions'):
-				(k, v)=child.keys()
-				# print(k,v)
-				# print(child.get(k),child.get(v))
-				if child.get(k).lower() == 'string' and \
-				   child.get(v).lower() == 'config':
-					#print(child.text)
-					child.text = ""
-	# print('\t'*depth, "#", sep='')
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
-root = ElementTree.parse(sys.stdin).getroot()
-walk(root,0)
-ElementTree.dump(root)
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+Version 0.2, written 2016-01-15 by rschmied@cisco.com
+"""
+
+
+import xml.etree.ElementTree as ET
+import errno, sys
+
+
+def main():
+	ET.register_namespace('', "http://www.cisco.com/VIRL")
+	virl_ns = "{http://www.cisco.com/VIRL}"
+
+	if len(sys.argv) == 2:
+		with open(str(sys.argv[1]).strip(), "r") as fh:
+			root = ET.parse(fh)
+	else:
+		root = ET.parse(sys.stdin)
+	
+	nodes = root.findall("{0}node".format(virl_ns))
+	for node in nodes:
+
+		# delete all config extensions
+		extensions = node.findall("{0}extensions".format(virl_ns))
+		for extension in extensions:
+			config_elem = extension.find("{0}entry[@key='config']".format(virl_ns))
+			if config_elem is not None:
+				extension.remove(config_elem)
+
+		# remove all address attributes from interfaces
+		# so ANK can start from a clean slate
+		interfaces = node.findall("{0}interface".format(virl_ns))
+		for attr in [ "ipv4", "ipv6", "netPrefixLenV4", "netPrefixLenV6" ]:
+			for interface in interfaces:
+				if interface.get(attr) is not None:
+					del interface.attrib[attr]
+
+	# write result to stdout
+	with sys.stdout as fh:
+		try:
+			root.write(fh, xml_declaration=True, encoding='UTF-8')
+		except IOError, e:
+			if e.errno == errno.EPIPE:
+				pass
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main())
 
